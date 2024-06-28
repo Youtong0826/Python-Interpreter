da9685330fba58aa37fb4fb5b406f409cbdb9843
Youtong0826 <jimmy2007826@gmail.com>
2024-06-29 05:53:02 +0800

    update visitor

--- src/core/visitor.py
+++ src/core/visitor.py
@@ -1,6 +1,24 @@
-from _ast_ import ASTNode
+from core._ast_ import ASTNode, BinaryOperatorNode
+from core._token import Token, Operator, DataType
 
 class NodeVisitor:
-    def __visit(node: ASTNode):
-        
+    def visit(self, node: ASTNode):
+        if isinstance(node, BinaryOperatorNode):
+            left: Token = self.visit(node.left)
+            right: Token = self.visit(node.right)
+            
+            match node.token.type:
+                case Operator.ADD:
+                    return Token(DataType.NUM, left.value + right.value)
+                
+                case Operator.SUB:
+                    return Token(DataType.NUM, left.value - right.value)
+                
+                case Operator.MUL:
+                    return Token(DataType.NUM, left.value * right.value)
+                
+                case Operator.DIV:
+                    return Token(DataType.NUM, left.value // right.value)
+            
+        return node.token
 
