876d5b3af348ae230670990532bd397b28ab9b1b
Youtong0826 <jimmy2007826@gmail.com>
2024-10-23 16:02:59 +0800

    rename

--- src/core/visitor.py
+++ src/core/visitor.py
@@ -9,16 +9,16 @@ class NodeVisitor:
             
             match node.token.type:
                 case Operator.ADD:
-                    return Token(DataType.NUM, left.value + right.value)
+                    return Token(DataType.INT, left.value + right.value)
                 
                 case Operator.SUB:
-                    return Token(DataType.NUM, left.value - right.value)
+                    return Token(DataType.INT, left.value - right.value)
                 
                 case Operator.MUL:
-                    return Token(DataType.NUM, left.value * right.value)
+                    return Token(DataType.INT, left.value * right.value)
                 
                 case Operator.DIV:
-                    return Token(DataType.NUM, left.value // right.value)
+                    return Token(DataType.INT, left.value // right.value)
             
         return node.token
 
